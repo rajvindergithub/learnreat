96f48582283ab2b57fdb6c75f61568fbe55d7a72
rajvindergithub <rajvindergithub@gmail.com>
2024-02-04 14:24:09 +0530

    master files

---
+++ learn-app/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+        <Header />
+
+        <Footer />    
+    </>
+  );
+}
+
+function Header(){
+    return <>
+        <h1>React App First Header</h1>    
+    </>
+}
+    
+function Footer(){
+    return <>
+        <h1>React App Footer First</h1>    
+    </>
+}
+
+export default App;
